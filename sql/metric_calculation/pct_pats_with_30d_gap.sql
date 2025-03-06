3375137da4539cb7e2a8746a5b51b5809539d69b
Sam Boss <97192036+boss-sam@users.noreply.github.com>
2025-03-06 12:04:09 -0500

    fix logic

--- sql/metric_calculation/pct_pats_with_30d_gap.sql
+++ sql/metric_calculation/pct_pats_with_30d_gap.sql
@@ -1,23 +1,68 @@
 BEGIN;
-with order_dates as (
-select 
+
+with fix_dates as (
+	select 
+		entity_id as person_id,
+		start_date,
+		coalesce(end_date, '9999-01-01'::date) as end_date
+	from 
+		{{ site }}_pedsnet.location_history
+),
+
+order_dates as (
+	select 
 		entity_id as person_id,
 		start_date,
 		end_date,
 		LAG(end_date) over (partition by entity_id order by start_date, end_date) as prev_end_date
 	from 
-		{{ site }}_pedsnet.location_history
-	where
-		start_date between '2009-01-01' and '2024-12-31'
+		fix_dates
 ),	
 
-get_numerator as (
-select 
-	count(distinct person_id) as ct_pats_with_30_day_gap
-from 
+group_dates as (
+	SELECT 
+	    	person_id, 
+	    	start_date, 
+	    	end_date,
+		prev_end_date,
+	        SUM(CASE WHEN start_date > prev_end_date THEN 1 ELSE 0 END)
+	           OVER (partition by person_id ORDER BY start_date) AS group_id
+    FROM 
 	order_dates
-where 
-	prev_end_date + interval '30 days' <= start_date
+),
+
+min_max_dates as (
+	SELECT 
+	    	person_id, 
+		group_id,
+	    	min(start_date) over (partition by person_id,group_id) as start_date, 
+		max(end_date) over (partition by person_id,group_id) as end_date, 
+		row_number() over (partition by person_id,group_id) as row_num
+    	FROM 
+		group_dates
+),
+
+prev_dates as (
+	select 
+		person_id,
+		start_date,
+		end_date,
+		LAG(end_date) over (partition by person_id order by start_date, end_date) as prev_end_date
+	from 
+		min_max_dates
+	where 
+		row_num = 1
+		and start_date > '2009-01-01'
+
+),
+
+get_numerator as (
+	select 
+		count(distinct person_id) as ct_pats_with_30_day_gap
+	from 
+		prev_dates
+	where 
+		prev_end_date + interval '30 days' < start_date
 ),
 
 get_denominator as (
